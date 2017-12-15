70e3c43702c5fec84d699c88e56134d654b76735
zhaiyu <foreverlosda@vip.qq.com>
2017-12-15 09:30:22 +0800

    first commit

---
+++ whatsThis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whatsThis
+//
+//  Created by 翟宇 on 2017/12/15.
+//  Copyright © 2017年 翟宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
